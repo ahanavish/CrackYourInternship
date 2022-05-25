d16b845a918b276f763ac595409d024aa691fcad
Ahana Vishwakarma <aha01na@gmail.com>
2022-05-25 15:06:13 +0530

    DAY 5

---
+++ Day5/LC560.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map <int, int> freq;
+        
+        int cum_sum=0, ans=0;
+        freq[0]=1;
+        for(int i=0; i<nums.size(); i++){
+            cum_sum += nums[i];
+    
+            if(freq.find(cum_sum-k) != freq.end())
+                ans += freq[cum_sum-k];
+            
+            freq[cum_sum]++;
+        }
+        
+        return ans;
+    }
+};
